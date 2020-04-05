f478761084bb74e9f41e1511f57d55d8fca1c9b8
ITXpert21 <47711428+ITXpert21@users.noreply.github.com>
2020-04-05 12:51:39 -0400

    2020/04/05-Firebase initialize and integration

--- src/config/constant.js
+++ src/config/constant.js
@@ -1,10 +1,10 @@
-export const FIREBASE_CLIENT_ID = '458247950527-46caisagu97ka58vjfvld9dmgb2g9j17.apps.googleusercontent.com';
-export const FIREBASE_REVERSED_CLIENT_ID = 'com.googleusercontent.apps.458247950527-46caisagu97ka58vjfvld9dmgb2g9j17';
-export const FIREBASE_API_KEY = 'AIzaSyDlgGSOngLav8STOwe3BZRcMqA5qz-YzFE';
-export const FIREBASE_PROJECT_ID = 'coronavirus-app-1fe73';
-export const FIREBASE_GCM_SENDER_ID = '458247950527';
-export const FIREBASE_BUNDLE_ID = 'com.tracking.Covid19';
+export const FIREBASE_CLIENT_ID = '444061209106-5jn0a83fa19jahl8cff75objd6h7t5qb.apps.googleusercontent.com';
+export const FIREBASE_REVERSED_CLIENT_ID = 'com.googleusercontent.apps.444061209106-5jn0a83fa19jahl8cff75objd6h7t5qb';
+export const FIREBASE_API_KEY = 'AIzaSyCWpsmpPKl4BGjetbbo7n_ITaQe2bamUZ8';
+export const FIREBASE_PROJECT_ID = 'cannago-ba078';
+export const FIREBASE_GCM_SENDER_ID = '444061209106';
+export const FIREBASE_BUNDLE_ID = 'com.cannagoapp.cannagrowdev';
 
-export const FIREBASE_STORAGE_BUCKET = 'coronavirus-app-1fe73.appspot.com';
-export const FIREBASE_GOOGLE_APP_ID = 'coronavirus-app-1fe73.appspot.com';
-export const FIREBASE_DATABASE_URL = 'https://coronavirus-app-1fe73.firebaseio.com';
+export const FIREBASE_STORAGE_BUCKET = 'cannago-ba078.appspot.com';
+export const FIREBASE_GOOGLE_APP_ID = '1:444061209106:ios:b56bc66758d61d47a1becb';
+export const FIREBASE_DATABASE_URL = 'https://cannago-ba078.firebaseio.com';
